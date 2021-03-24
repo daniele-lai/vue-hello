227d0275ffd51a96b67b911afa225049f8875578
Daniele <danielelai86@gmail.com>
2021-03-24 16:33:45 +0100

    Aggiunta funzione

--- js/script.js
+++ js/script.js
@@ -7,7 +7,13 @@ var app = new Vue({
   el: '#root',
   data: {
     message: "Hello Boolean Teachers!",
-    img: "img/memevue.jpg"
+    img: "img/memevue.jpg",
+    mieClassi: "hide"
+  },
+  methods: {
+    funzioneImg: function () {
+      this.mieClassi = "show";
+    }
   }
 }
 );
